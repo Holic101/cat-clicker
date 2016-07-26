2a86828501bc72597f4d10fe1724e5142dfecc5a
Holic101 <dietrich.kriworotow@gmail.com>
2016-07-26 09:19:36 +0200

    final version

---
+++ js/cat-clicker.js
@@ -0,0 +1,117 @@
+//Model
+  var model = {
+    currentCat: null,
+    cats : [
+          {
+            name: "Cutie Pie",
+            clicks: 0,
+            path: "img/cute_cat0.jpg"
+          },
+          {
+            name: "Little Beaver",
+            clicks: 0,
+            path: "img/cute_cat1.jpg"
+          },
+          {
+            name: "Kitty",
+            clicks: 0,
+            path: "img/cute_cat2.jpg"
+          },
+          {
+            name: "Mautzi",
+            clicks: 0,
+            path: "img/cute_cat3.jpg"
+          },
+          {
+            name: "Schnurri",
+            clicks: 0,
+            path: "img/cute_cat4.jpg"
+          }
+  ]
+  };
+
+//Controller
+  var controller = {
+
+    init: function() {
+      //set the current cat to the first cat in the list
+      model.currentCat = model.cats[0];
+      //initialize the views
+      viewButtons.init();
+      viewCats.init();
+    },
+
+    countClicks: function() {
+      model.currentCat.clicks++;
+      viewCats.render();
+    },
+
+    displayCurrentCat: function() {
+      //tells the Cat View to change the picture, the name and the click number
+      return model.currentCat;
+    },
+
+    getCats: function() {
+      return model.cats;
+    },
+
+    setCurrentCat: function(cat) {
+      model.currentCat = cat;
+    }
+  };
+  // Button View
+  var viewButtons = {
+    init: function() {
+      this.catList = document.getElementById("cat-list");
+      this.render();
+    },
+
+    render: function() {
+      var cats = controller.getCats(),
+          cat, i;
+
+      for(i = 0; i<cats.length; i++) {
+        cat = cats[i];
+        this.listElement = document.createElement("li");
+        this.listElement.textContent = cats[i].name;
+        //Attach an event listener to each cat name
+        this.listElement.addEventListener("click", (function(catCopy) {
+                return function() {
+                  controller.setCurrentCat(catCopy);
+                  viewCats.render();
+                };
+        })(cat));
+
+        this.catList.appendChild(this.listElement);
+      }
+    }
+  };
+// Cat picture and counter view
+  var viewCats = {
+
+    init: function() {
+      //put the DOM elements into handy pointers for reference
+      this.catWrapper = document.getElementById("cat-wrapper");
+      this.catName = document.getElementById("cat-name");
+      this.catCount = document.getElementById("cat-count");
+      this.catImg = document.getElementById("cat-img");
+      this.catImg.addEventListener("click", function() {
+        controller.countClicks();
+      });
+
+      this.render();
+    },
+
+    render: function() {
+      var currentCat = controller.displayCurrentCat();
+      this.catName.textContent = currentCat.name;
+      this.catCount.textContent = currentCat.clicks;
+      this.catImg.src = currentCat.path;
+    },
+
+    clickHandler: function(catId) {
+      controller.countClicks(catId);
+    }
+  };
+
+  controller.init();
--- scripts/cat-clicker.js
+++
@@ -1,9 +0,0 @@
-var clickCounter = 0;
-$(function () {
-  console.log("test");
-
-  $("img").click(function() {
-    clickCounter++;
-    $("#clicker").html(clickCounter);
-  });
-})
